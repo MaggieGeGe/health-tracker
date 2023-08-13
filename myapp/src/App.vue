5a48a517628cfbd37277d991e8940fbb1d26245b
Diaove <384821395@qq.com>
2023-08-13 18:46:33 +0900

    update frontend

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -10,37 +10,33 @@
     <div style="display: flex">
 
 
-      <div style="width:200px; overflow: hidden;margin-right: 4px;background-color: #f3ebf1">
+      <div style="width:220px; overflow: hidden;margin-left: 5px;margin-right: 4px;background-color: #f3ebf1">
         <el-menu :default-active="$router.path" router class="el-menu-demo" @select="handleSelect">
           <el-menu-item index="/">
-              <el-icon color="#000001"><Menu /></el-icon>
-              MainPage
+              <el-icon color="#000000"><Menu /></el-icon>
+              <span>MainPage</span>
           </el-menu-item>
           <el-sub-menu>
             <template #title>
               <el-icon><Refrigerator /></el-icon>
-              GroceryInfo
+              <span>GroceryInfo</span>
             </template>
             <el-menu-item index="/grocery">
               <el-icon><ShoppingCartFull /></el-icon>
-              Grocery One
+              <span>Grocery One</span>
             </el-menu-item>
             <el-menu-item index="">
-              <el-icon><ShoppingCartFull /></el-icon>
-              Grocery Two
+              <el-icon><ShoppingCartFull /></el-icon>Grocery Two
             </el-menu-item>
             <el-menu-item index="">
-              <el-icon><ShoppingCartFull /></el-icon>
-              Grocery Three
+              <el-icon><ShoppingCartFull /></el-icon>Grocery Three
             </el-menu-item>
           </el-sub-menu>
           <el-menu-item index="/requiredfood">
-            <el-icon><Star /></el-icon>
-            Required Food
+            <el-icon><Star /></el-icon>Required Food
           </el-menu-item>
           <el-menu-item index="/caloriesquery">
-            <el-icon><View /></el-icon>
-            Calories Query
+            <el-icon><View /></el-icon>Calories Query
           </el-menu-item>
           <el-menu-item index="/recipe">
             <el-icon><ChatLineSquare /></el-icon>
@@ -53,7 +49,7 @@
         </el-menu>
       </div>
       <!-- body data-->
-      <div style="flex:1;background-color: #d1ddf1; padding:10px">
+      <div style="flex:1;background-color: #d1ddf1; padding:10px;margin-right: 10px">
         <router-view/>
       </div>
 
@@ -63,8 +59,16 @@
 </template>
 
 <script>
-import {Menu} from "@element-plus/icons-vue";
+import {
+  Menu,
+  ChatLineSquare,
+  User,
+  View,
+  Star,
+  ShoppingCartFull,
+  Refrigerator
+} from "@element-plus/icons-vue";
 export default {
-  components: {Menu}
+  components: {Menu,ChatLineSquare,User,View,Star,ShoppingCartFull,Refrigerator}
 }
 </script>
--- myapp/src/views/GroceryView.vue
+++ myapp/src/views/GroceryView.vue
@@ -1,32 +1,83 @@
 <template>
   <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">Mainpage</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/grocery' }">GroceryInfo</el-breadcrumb-item>
+    </el-breadcrumb>
     <!--search list-->
-    <div>
-      <el-input style="width: 150px" placeholder="Please input the food name"></el-input>
-      <el-input style="width: 150px;margin-left: 10px" placeholder="Please input the type"></el-input>
+    <div style="margin-top: 10px">
+      <el-input
+          style="width: 250px"
+          placeholder="Please input the food name"
+          clearable
+      >
+        <template #suffix><el-icon><Search /></el-icon></template>
+      </el-input>
+      <el-input
+          style="width: 250px;margin-left: 10px"
+          placeholder="Please input the type"
+      >
+        <template #suffix><el-icon><Search /></el-icon></template>
+      </el-input>
       <el-button style="margin-left: 10px" type="primary" round>Search</el-button>
     </div>
-    <el-table :data="tableData" style="margin-top: 10px" stripe >
-      <el-table-column prop="name" label="Name" width="100px"></el-table-column>
-      <el-table-column prop="type" label="Type" width="100px"></el-table-column>
-      <el-table-column prop="productiondate" label="Production Date"></el-table-column>
-      <el-table-column prop="expireddate" label="Expired Date"></el-table-column>
-      <el-table-column prop="nutrition" label="Nutrition"></el-table-column>
+    <div style="padding: 10px 0">
+      <el-button type="primary" style="width: 100px;">Add <el-icon><CirclePlus /></el-icon></el-button>
+      <el-button type="danger" style="width: 100px;">Delete <el-icon><CircleClose /></el-icon></el-button>
+      <el-button type="primary" style="width: 100px;">Upload <el-icon><Upload /></el-icon></el-button>
+      <el-button type="primary" style="width: 100px;">Download <el-icon><Download /></el-icon></el-button>
+    </div>
+    <el-table :data="tableData" style="margin-top: 10px;margin-left: 0px" stripe header-cell-class-name="headerBg">
+      <el-table-column prop="name" label="Name" width="150px"></el-table-column>
+      <el-table-column prop="type" label="Type" width="80px"></el-table-column>
+      <el-table-column prop="productiondate" label="Production Date"  width="140px"></el-table-column>
+      <el-table-column prop="expireddate" label="Expired Date"  width="120px"></el-table-column>
+      <el-table-column prop="nutrition" label="Nutrition"  width="120px"></el-table-column>
       <el-table-column prop="amount" label="Amount" width="80px"></el-table-column>
-      <el-table-column prop="remainingday" label="Remaining Days"></el-table-column>
+      <el-table-column prop="remainingday" label="Remaining Days"  width="140px"></el-table-column>
+      <el-table-column label="Operation">
+          <el-button type="success" style="width: 70px;">Edit <el-icon><Edit /></el-icon></el-button>
+          <el-button type="danger" style="width: 70px;">Delete <el-icon><CircleClose /></el-icon></el-button>
+      </el-table-column>
     </el-table>
     <div style="margin-top: 30px">
-      <el-pagination background page-size="10" layout="prev, pager, next" :total="tableData.length" />
+      <el-pagination
+          v-model:current-page="currentPage3"
+          v-model:page-size="pageSize3"
+          :page-sizes="[10,20,50,100, 200, 300, 400]"
+          :small="small"
+          :disabled="disabled"
+          :background="background"
+          layout="sizes, prev, pager, next, jumper"
+          :total="1000"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
 <script>
+
+import {CircleClose, Search} from "@element-plus/icons-vue";
 export default {
   name: "requiredView",
+  components: {CircleClose, Search},
   data() {
+    const generateExampleData = () => ({
+      name: "Example Food",
+      type: "Type",
+      productiondate: "2023-07-01",
+      expireddate: "2023-07-10",
+      nutrition: "Nutrition",
+      amount: Math.floor(Math.random() * 10) + 1,
+      remainingday: Math.floor(Math.random() * 10) + 1
+    });
+
     return {
-      tableData: []
-    }
+      tableData: Array.from({ length: 20 }, generateExampleData),
+      headerBg:"headerBg"
+    };
+
   }
   // created(){
   //   this.load()
@@ -41,3 +92,8 @@ export default {
   // }
 }
 </script>
+<style>
+.headerBg {
+  background-color: #edf3fc !important;
+}
+</style>
